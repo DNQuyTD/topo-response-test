48ba7e714bca473b4963375a1b593dd652f24b6c
Ken <87507882+DNQuyTD@users.noreply.github.com>
2023-10-24 16:44:23 +0700

    /route/graph/finished-and-lose/user/:id

--- src/routes/index.js
+++ src/routes/index.js
@@ -76,5 +76,6 @@ router.get('/timeline/subArea/:id', responseController.getResponse);
 router.get('/timeline/route/:id', responseController.getResponse);
 router.get('/timeline/user/:id', responseController.getResponse);
 router.post('/area/:id/check-in', responseController.getResponse);
+router.get('/route/graph/finished-and-lose/user/:id', responseController.getResponse);
 
 module.exports = router;
