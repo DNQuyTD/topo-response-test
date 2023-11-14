dfdcdf520865de04bffda325695993e39e8573ea
Ken <87507882+DNQuyTD@users.noreply.github.com>
2023-11-14 11:11:45 +0700

    report

--- src/routes/index.js
+++ src/routes/index.js
@@ -88,4 +88,5 @@ router.delete('/route/:id/project', responseController.getResponse);
 router.get('/merchandise/list/purchased/route/:id', responseController.getResponse);
 router.get('/area/download/:id/merchandise/:id1', responseController.getResponse);
 router.get('/merchandise/list/purchased/area/:id', responseController.getResponse);
+router.post('/report', responseController.getResponse);
 module.exports = router;
